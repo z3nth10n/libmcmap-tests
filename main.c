0c5d430bcce9b89a886a6389b75c018c1521d413
z3nth10n <z3nth10n@gmail.com>
2019-09-26 16:51:57 +0200

    Combining path

--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <windows.h>
 
 #include "mcmap.h"
+#define nullptr ((void*)0)
 
 int main()
 {
     // printf("Hello world!\n");
-    mcmap_region_read(0, 0, "../Worlds/world_0/region/");
+     char cwd[PATH_MAX];
+     char filename[] = "Worlds/world_0/region/";
+
+     getcwd(cwd, sizeof(cwd));
+
+   /* if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+   } else {
+       perror("getcwd() error");
+       return 1;
+   }*/
+
+   GetFullPathName(filename, MAX_PATH, cwd, nullptr);
+
+    mcmap_region_read(0, 0, cwd);
     getchar();
     return 0;
 }
